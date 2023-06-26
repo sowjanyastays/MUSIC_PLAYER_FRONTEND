71a53809d51825fa59f9b405a5bf143fbb7c9397
Sowjanya <sowjanyastays@gmail.com>
2023-06-26 12:04:27 +0530

    Added song gallery and adding file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import FileUploadComponent from './components/FileUploadComponent/FileUploadComponent';
+import SongGalleryComponent from './components/SongGalleryComponent/SongGalleryComponent';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     
+     <div className='container'>
+     <h1 id='heading'>Song Player</h1>
+        <nav className='nav-menu'>
+          <Link to='/'>Home</Link>
+          <Link to='/admin/upload'>Add Songs</Link>
+        </nav>
+        <Routes>
+          <Route exact path='/' element={<SongGalleryComponent />} />
+          <Route path='/admin/upload' element={<FileUploadComponent />} />
+        </Routes>
+     </div>
     </div>
+    </Router>
   );
 }
 
